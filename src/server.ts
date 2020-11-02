1b16810e0e5d38f887119fb663c7b6d468e28069
Mariana Casetta <casettamariana.ufg@gmail.com>
2020-11-02 19:32:33 -0300

    Base do servidor, ainda com bugs

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,41 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanage';
+
+import './database/connection';
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/users/:id', (request, response) => {
-    console.log(request.query);
-    console.log(request.params);
-    console.log(request.body);
+app.post('/orphanages', async (request, response) => {
+    const {
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+    } = request.body;
+
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanage = orphanagesRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+    });
+
+    await orphanagesRepository.save(orphanage);
     
     return response.json({ message: "Hello Word" });
 });
 
 app.listen(3333);
+
+// 56min23seg
